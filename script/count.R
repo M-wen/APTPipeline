8794def79f48a04a0a5db6cdeffbea8c6433f5fe
M-wen <54365182+M-wen@users.noreply.github.com>
2024-08-28 15:42:34 +0800

    Add files via upload

--- script/count.R
+++ script/count.R
@@ -16,10 +16,12 @@ seurat_data <- Read10X(args[1],gene.column = 1)
 
 #expr<-filter[["RNA"]]@counts
 
-#APT<-rownames(seurat_data)[which(str_detect(rownames(seurat_data),"APT_"))]
-#expr<-seurat_data[APT,]
-expr<-seurat_data[which(rownames(seurat_data) %in% c("PTK7_APT","Contr","split_APT")),]
+APT<-rownames(seurat_data)[which(str_detect(rownames(seurat_data),"APT"))]
+expr<-seurat_data[APT,]
+
 expr2<-t(as.data.frame(expr))
 write.table(expr2,file =paste(args[2],args[3],"_count.csv",sep=""),row.names=T,sep="\t",quote=F)
+
+
 
 #expr<-expr%>%filter(rownames(expr)%in%c("Contr","CD318_APT","CD49C_APT","CD71_APT","EPCAM_APT","MET_APT","PD_L1_APT","PRNP_APT","PTK7_APT","PTPRF_APT","ZNRF3_APT"))
--- script/count_sum.R
+++ script/count_sum.R
@@ -5,19 +5,25 @@ args<-commandArgs(T)
 
 
 
-oligosumm<-data.table::fread(paste(args[1],"/02.APT/count/Oligo_APT_ALL_barcode_count.csv",sep=""))
+oligosumm<-data.table::fread(args[1])
 
-#barcode<-data.table::fread(paste(args[1],"/01.scRNA/",args[2],"/02.count/",args[2],"_combined_list.txt",sep=""),header=F)
-barcode<-data.table::fread(paste(args[1],"/01.scRNA/",args[2],"/02.count/","barcodeTranslate.txt",sep=""),header=F)
+#barcode<-data.table::fread(paste(args[3],"/01.scRNA/",args[4],"/02.count/",args[4],"_combined_list.txt",sep=""),header=F)
+
+barcode<-data.table::fread(paste(args[3],"/01.scRNA/",args[4],"/02.count/","barcodeTranslate.txt",sep=""),sep="\t",header=F)
 
 colnames(barcode)<-c("V1","cell")
-
+cell <- data.table::fread(paste(args[3],"/01.scRNA/",args[4],"/02.count/","beads_barcodes.txt",sep=""),header=F)
+#colnames(barcode)<-c("V1")
+cell<- as.data.frame(cell)
+cell1 <- cell$V1
+filter <- barcode %>% filter(barcode$cell %in% cell1)
+barcode <- filter
 
 APT<-inner_join(barcode,oligosumm,by="V1")
 
 APT$type<-"oligo"
 
-cdna<-data.table::fread(paste(args[1],"/02.APT/count/CDNA_APT_ALL_barcode_count.csv",sep=""))
+cdna<-data.table::fread(args[2])
 
 
 APTcdna<-inner_join(barcode,cdna,by="V1")
@@ -26,8 +32,7 @@ APTcdna<-inner_join(barcode,cdna,by="V1")
 APTcdna$type<-"cdna"
 
 
-#colnam<-colnames(APT)[which(stringr::str_detect(colnames(APT),"APT"))]
-colnam<-c("PTK7_APT", "Contr", "split_APT")
+colnam<-colnames(APT)[which(stringr::str_detect(colnames(APT),"APT"))]
 
 col<-c("V1","cell","type",colnam)
 
@@ -81,14 +86,13 @@ allAPT<-rbind(APT,APTcdna)
 
 #all<-aggregate(cbind(`PTK7-APT`,`MET-APT`,`CD71-APT`,`PD-L1-APT`,`CD49C-APT`,`PTPRF-APT`,`CD318-APT`,`EPCAM-APT`,`ZNRF3-APT`,`Contr`)~cell,allAPT,sum)
 
-validCell <- as.data.frame(data.table::fread(paste(args[1],"/01.scRNA/",args[2],"/02.count/","beads_barcodes.txt",sep=""),header=F))
+
 
 allAPT2<-allAPT[,-c(1,3)]
-allAPT2 <- allAPT2[which(allAPT2$cell %in% validCell$V1),]
 
 all<-aggregate(.~cell,allAPT2,sum)
 
-write.csv(all,file=paste(args[1],"/02.APT/count/ALL_APT_cell.csv",sep=""),quote=F,row.names=F)
+write.csv(all,file=paste(args[3],"/02.APT/count/ALL_APT_cell.csv",sep=""),quote=F,row.names=F)
 
 
 mean<-round(colMeans(all[,-1]),0)
@@ -102,7 +106,7 @@ count<-as.data.frame(count)
 count$APT<-rownames(count)
 count$cellnumber<-cellnumber
 
-count$sample<-args[2]
+count$sample<-args[4]
 count<-count[,c(5,3,4,2,1)]
 
-write.table(count,file=paste(args[1],"/02.APT/count/Count_mean.txt",sep=""),sep="\t",row.names=F,quote=F)
+write.table(count,file=paste(args[3],"/02.APT/count/Count_mean.txt",sep=""),sep="\t",row.names=F,quote=F)
